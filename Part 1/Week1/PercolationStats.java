e8d7718c4507f84daea110c2320a9b5f2d65a15e
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2022-01-17 23:52:24 -0800

    edit

--- Part 1/Week1/PercolationStats.java
+++ Part 1/Week1/PercolationStats.java
@@ -1,5 +1,10 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
+
+import edu.princeton.cs.algs4.Stopwatch;
+
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class PercolationStats {
   private double[] res;
@@ -14,12 +19,17 @@ public class PercolationStats {
     res = new double[trials];
 	  for (int i = 0; i < trials; i++) {
       Percolation p = new Percolation(n); 
+      int cnt = 0;
       while (!p.percolates()) {
-        int row = StdRandom.uniform(1,n);
-        int col = StdRandom.uniform(1,n);
-        p.open(row, col);
+        int row = StdRandom.uniform(n) + 1;
+        int col = StdRandom.uniform(n) + 1;
+        if (!p.isOpen(row, col)) {
+          p.open(row, col);
+          cnt++;
+        }
+        
       }
-      res[i] = (double)p.numberOfOpenSites() / N;
+      res[i] = ((double) cnt) / N;
 	  }
     mean = StdStats.mean(res);
     stddev = StdStats.stddev(res);
@@ -47,6 +57,22 @@ public class PercolationStats {
 
    // test client (see below)
    public static void main(String[] args) {
+    StdOut.printf("%-25s\n", "Please input 2 integers");
+		int N = StdIn.readInt();
+		int T = StdIn.readInt();
+		
+		Stopwatch wt = new Stopwatch();
+		
+		PercolationStats ps = new PercolationStats(N, T);
+		
+		// elapsed CPU time in seconds
+		double elapsed = wt.elapsedTime();
+		
+		StdOut.printf("%-25s= %.15f\n", "elapsed CPU time", elapsed);
+		StdOut.printf("%-25s= %.7f\n", "mean", ps.mean());
+		StdOut.printf("%-25s= %.17f\n", "stddev", ps.stddev());
+		StdOut.printf("%-25s= [%.15f, %.15f]\n", "%95 confidence interval", 
+				ps.confidenceLo(), ps.confidenceHi());
 
    }
 
