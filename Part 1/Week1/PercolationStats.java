a228cb6174b48cd0062947c5a78426647ff28b72
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2022-01-17 23:36:38 -0800

    Update PercolationStats.java

--- Part 1/Week1/PercolationStats.java
+++ Part 1/Week1/PercolationStats.java
@@ -2,30 +2,52 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class PercolationStats {
-	
+  private double[] res;
+  private double mean;
+  private double stddev;
     // perform independent trials on an n-by-n grid
-    public PercolationStats(int n, int trials) {
-		Percolation p = new Percolation(n); 
-		for (int i = 0; i < trials; i++) {
-
-		}
-
-		
+  public PercolationStats(int n, int trials) {
+    if (n <= 0 || trials <= 0) {
+      throw new IllegalArgumentException();
+    }
+    int N = n * n;
+    res = new double[trials];
+	  for (int i = 0; i < trials; i++) {
+      Percolation p = new Percolation(n); 
+      while (!p.percolates()) {
+        int row = StdRandom.uniform(1,n);
+        int col = StdRandom.uniform(1,n);
+        p.open(row, col);
+      }
+      res[i] = (double)p.numberOfOpenSites() / N;
+	  }
+    mean = StdStats.mean(res);
+    stddev = StdStats.stddev(res);
 	}
 
     // sample mean of percolation threshold
-    public double mean() 
+    public double mean() {
+      return mean;
+    }
 
     // sample standard deviation of percolation threshold
-    public double stddev()
+    public double stddev() {
+      return stddev;
+    }
 
     // low endpoint of 95% confidence interval
-    public double confidenceLo()
+    public double confidenceLo() {
+      return mean - 1.96 * stddev / Math.sqrt(res.length);
+    }
 
     // high endpoint of 95% confidence interval
-    public double confidenceHi()
+    public double confidenceHi() {
+      return mean + 1.96 * stddev / Math.sqrt(res.length);
+    }
 
    // test client (see below)
-   public static void main(String[] args)
+   public static void main(String[] args) {
+
+   }
 
 }
